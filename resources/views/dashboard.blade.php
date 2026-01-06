d55e51db9410531f91f03ee71349a94dee5a5bd9
jackytv12 <jackyavenido.tv@gmail.com>
2026-01-06 21:10:48 +0800

    added

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container py-4">
+  <div class="card">
+    <div class="card-body">
+      <h1 class="h4 mb-2">Dashboard</h1>
+      <p class="mb-0">You’re logged in.</p>
+    </div>
+  </div>
+</div>
+@endsection
