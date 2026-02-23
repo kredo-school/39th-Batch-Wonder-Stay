ce4d4d36a0fdb00f13a306d214cf2b1f6e9b1a09
Kosuke Okumoto <kosukeokumoto913@gmail.com>
2026-02-23 22:50:39 +0900

    adjustments

--- app/Services/TranslationService.php
+++ app/Services/TranslationService.php
@@ -7,35 +7,35 @@ use App\Models\Translation;
 
 class TranslationService
 {
-    public function translate(string $key, string $text,string $locale): string
-    {
-        $key = $text;
+    // public function translate(string $key, string $text,string $locale): string
+    // {
+    //     $key = $text;
 
-        $cached = Translation::where('key', $key)
-            ->where('locale', $locale)
-            ->first();
+    //     $cached = Translation::where('key', $key)
+    //         ->where('locale', $locale)
+    //         ->first();
 
-        if ($cached) {
-            return $cached->text;
-        }
-        $response = Http::post('http://localhost:5001/translate', [
-            'q' => $text,
-            'source' => 'en',
-            'target' => $locale,
-            'format' => 'text',
-        ]);
+    //     if ($cached) {
+    //         return $cached->text;
+    //     }
+    //     $response = Http::post('http://localhost:5001/translate', [
+    //         'q' => $text,
+    //         'source' => 'en',
+    //         'target' => $locale,
+    //         'format' => 'text',
+    //     ]);
 
-        $translated = $response->json('translatedText') ?? $text;
-            Translation::updateOrCreate(
-            [
-                'key' => $key,
-                'locale' => $locale,
-            ], 
-            [
-                'text' => $translated,
-            ]
-       );
+    //     $translated = $response->json('translatedText') ?? $text;
+    //         Translation::updateOrCreate(
+    //         [
+    //             'key' => $key,
+    //             'locale' => $locale,
+    //         ], 
+    //         [
+    //             'text' => $translated,
+    //         ]
+    //    );
 
-        return $translated;
-    }
+    //     return $translated;
+    // }
 }
